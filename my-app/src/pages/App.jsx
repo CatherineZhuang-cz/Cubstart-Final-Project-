c700152f5c29d71af4928e3987ec2ed0e49d608b
CatherineZhuang-cz <lynn_kkcz@berkeley.edu>
2024-11-21 03:47:54 -0800

    Initial commit

---
+++ my-app/src/pages/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to Trivia</h1>
+    </div>
+  );
+};
+
+export default App;
